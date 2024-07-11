b249182506c668dae1b9e55be8dc863e3ac0e3a4
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-11 10:00:51 +0300

    Check Band lifespan

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- ranks country origins of bands
+-- SET life_span = formed - split
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
