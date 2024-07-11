8e89e437b3e78de433761a9dba8e73546cde84ad
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-11 20:27:20 +0300

    Calculate average score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,19 @@
+-- Calculate the average score for the given user_id
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score DECIMAL(10, 2);
+
+    -- Calculate the average score for the given user_id
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE corrections.user_id = user_id;
+
+    -- Update the user's average_score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END//
+
+DELIMITER ;
