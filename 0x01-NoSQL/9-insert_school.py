300abf236f9d96b92789c3cc237727d97762671a
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-16 08:28:39 +0300

    Insetr Documment

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Function that changes all topics of a
+    school document based on the name
+"""
+def insert_school(mongo_collection, **kwargs):
+    """
+    Function that changes all topics of a
+    school document based on the name
+    """
+    insert = mongo_collection.insert_one(kwargs)
+    return insert.inserted_id
