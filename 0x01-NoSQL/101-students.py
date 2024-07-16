8a6cf1e2989e97d15ceac5c76acf4693104e1497
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-16 15:45:19 +0300

    Project and filter

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,15 +6,13 @@ def top_students(mongo_collection):
     """
     Sorts students according to avag score
     """
-    avg_score = 0
-    for student in mongo_collection.find({}):
-        avg_score = 0
-        for sub in student.get("topics"):
-            avg_score += sub.get("score", 0)
-
-        if avg_score:
-            mongo_collection.update_one(
-                {"name": student.get("name")},
-                {"$set": {"averageScore": avg_score / 3}},
-            )
-    return list(mongo_collection.aggregate([{"$sort": {"averageScore": -1}}]))
+    return mongo_collection.aggregate([
+        {
+            "$project" : {"_id": 1,
+                          "name": 1,
+                          "averageScore": {"$avg" : "$topics.score"}
+                          }
+        },
+        { "$sort":
+                {"averageScore": -1}
+        }])
