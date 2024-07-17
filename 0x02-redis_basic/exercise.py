d024aaf22ba5691038be39b7011aaa97328be7b5
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-17 15:39:58 +0300

    Update time string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -129,19 +129,33 @@ def replay(method: Callable) -> None:
     """
     Display the history of calls of a particular function.
     """
-    r = redis.Redis()
-    inputs = r.lrange(f"{method.__qualname__}:inputs", 0, -1)
-    outputs = r.lrange(f"{method.__qualname__}:outputs", 0, -1)
-    count = len(input)
-    times_str = "times"
-    if count == 1:
-        times_str = "times"
-    print(
-        f"{method.__qualname__} was called {count
-                                            .decode('utf-8')} {times_str}: "
-    )
+    inputs = cache._redis.lrange(f"{method.__qualname__}:inputs", 0, -1)
+    outputs = cache._redis.lrange(f"{method.__qualname__}:outputs", 0, -1)
+    count = cache.get(method.__qualname__)
+    print(f"{method.__qualname__} was called {count.decode('utf-8')} times:")
 
     for input_key, output_key in zip(inputs, outputs):
         input_str = input_key.decode("utf-8")
         output_str = output_key.decode("utf-8")
         print(f"{method.__qualname__}(*({input_str},)) -> {output_str}")
+
+# def replay(method: Callable) -> None:
+#     """
+#     Display the history of calls of a particular function.
+#     """
+#     r = redis.Redis()
+#     inputs = r.lrange(f"{method.__qualname__}:inputs", 0, -1)
+#     outputs = r.lrange(f"{method.__qualname__}:outputs", 0, -1)
+#     count = len(input)
+#     times_str = "times"
+#     if count == 1:
+#         times_str = "times"
+#     print(
+#         f"{method.__qualname__} was called {count
+#                                             .decode('utf-8')} {times_str}: "
+#     )
+
+#     for input_key, output_key in zip(inputs, outputs):
+#         input_str = input_key.decode("utf-8")
+#         output_str = output_key.decode("utf-8")
+#         print(f"{method.__qualname__}(*({input_str},)) -> {output_str}"
