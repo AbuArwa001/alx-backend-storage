3aa81040367ab8c20bd2024689b09434a5960fe4
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-17 14:07:37 +0300

    Add dco str

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,29 @@
 #!/usr/bin/env python3
 """
 Module contains a class to cache redis
+Author: Khalfan Athman
 """
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Prototype: def count_calls(method: Caallable) -> Callable:
+    Returns a Callable
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """
+        Prototype: def wrapper(self, *args, **kwds):
+        Returns wrapper
+        """
+        key_m = method.__qualname__
+        self._redis.incr(key_m)
+        return method(self, *args, **kwds)
+    return wrapper
 
 class Cache:
     """
@@ -16,10 +34,12 @@ class Cache:
         Initialize the Cache class.
 
         This initializes a connection to a Redis database.
+        Author: Khalfan Athman
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in the Redis cache and return the key.
@@ -30,6 +50,7 @@ class Cache:
 
         Returns:
         str: The key under which the data is stored.
+        Author: Khalfan Athman
         """
         key = str(uuid.uuid4())  # Ensure key is a string
         self._redis.set(key, data)
