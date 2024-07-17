8dd75b64e1741a7ba60a692c21b18ea7199fbdc9
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-17 20:45:18 +0300

    Get request count

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,27 +2,33 @@
 """
 Module containing a get_page function
 """
-import redis
 import requests
-import time
+import redis
+import functools
 
 # Initialize Redis connection
 redis_conn = redis.Redis()
 
 
-def get_page(url: str) -> str:
-    """
-    Get page for link
-    """
-    # Check if URL access count exists in Redis
-    count_key = f"count:{url}"
-    url_count = redis_conn.get(count_key)
-    if url_count:
-        redis_conn.incr(count_key)
-    else:
-        redis_conn.set(count_key, 1, ex=10)  # Cache with 10 second expiration
+def track_url_access(func):
+    @functools.wraps(func)
+    def wrapper(url):
+        # Track URL access count in Redis
+        count_key = f"count:{url}"
+        url_count = redis_conn.get(count_key)
+        if url_count:
+            redis_conn.incr(count_key)
+        else:
+            redis_conn.set(
+                count_key, 1, ex=10
+            )  # Cache with 10 second expiration
+        return func(url)
 
-    # Fetch page content using requests module
+    return wrapper
+
+
+@track_url_access
+def get_page(url: str) -> str:
     try:
         response = requests.get(url)
         response.raise_for_status()  # Raise exception for bad status codes
