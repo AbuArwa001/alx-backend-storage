969e624b04d991ccfb176cf83f89a19b5fca0a37
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-16 14:56:20 +0300

    Count requests

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python3
-"""
-Module containing a get_page function
-"""
-import requests
+'''A module with tools for request caching and tracking.
+'''
 import redis
-import functools
-
-# Initialize Redis connection
-redis_conn = redis.Redis()
-
-
-def track_url_access(func):
-    """
-    function  tracking url access
-    """
-    @functools.wraps(func)
-    def wrapper(url):
-        """
-        Wrapper function Track URL access count in Redis
-        """
-        # Track URL access count in Redis
-        count_key = f"count:{url}"
-        url_count = redis_conn.get(count_key)
-        if url_count:
-            redis_conn.incr(count_key)
-        else:
-            redis_conn.set(
-                count_key, 1, ex=10
-            )  # Cache with 10 second expiration
-        return func(url)
-
-    return wrapper
+import requests
+from functools import wraps
+from typing import Callable
 
 
-@track_url_access
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """
-    get_page function  tracking url access
-    """
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
